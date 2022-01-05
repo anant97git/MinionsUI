ebf238e44e95b077a1d3381f9396561bd9f37dc2
jain1197 <anant.jain@timesinternet.in>
2022-01-05 23:25:20 +0530

    some work done

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -12,13 +12,52 @@ import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import './TopNav.css';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 const pages = [];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 const TopNav = () => {
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
+  const [anchorElNav, setAnchorElNav] = useState(null);
+  const [anchorElUser, setAnchorElUser] = useState(null);
+
+  const [isLoggedIn, setIsLoggedIn] = useState(0);
+
+  const [username, setUsername] = useState(null);
+
+  // api for validating user :- https://jsso.indiatimes.com/sso/crossdomain/v1liteUserProfile?responsetype=json&type=JSON&update=true&siteId=eec5b06ed436ddefdb4c3a59c5ea0468&ticketId=
+
+  useEffect(() => {
+
+    console.log(window.location.href)
+
+    if (window.location.href.includes("ticketId") && window.location.href.includes("status")) {
+
+      const queryParams = new URLSearchParams(window.location.search);
+      const ticketId = queryParams.get('ticketId');
+      const channel = queryParams.get('channel')
+      const site = queryParams.get('site');
+
+      console.log(ticketId)
+
+      const getUserDetailApi = 'http://jssostg.indiatimes.com/sso/crossdomain/v1liteUserProfile?responsetype=json&type=JSON&update=true&siteId=eec5b06ed436ddefdb4c3a59c5ea0468&channel=minions&ticketId=' + ticketId;
+      console.log(getUserDetailApi)
+      console.log(getUserDetailApi)
+
+      fetch(getUserDetailApi)
+        .then(response => console.log(response))
+        .catch(error => console.log(error))
+
+      console.log("--- " + ticketId);
+
+
+
+      setIsLoggedIn(1)
+    }
+
+  }, [window.location.href])
+
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
@@ -34,6 +73,12 @@ const TopNav = () => {
   const handleCloseUserMenu = () => {
     setAnchorElUser(null);
   };
+
+  const loggedInClicked = () => {
+    setIsLoggedIn(1);
+  }
+
+
 
   return (
     <AppBar position="static" id="appbar">
@@ -58,14 +103,26 @@ const TopNav = () => {
 
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
-              {/* <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}> */}
+
+              {/* <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
               <IconButton sx={{ p: 0 }}>
                 <a
                   href="https://jssostg.indiatimes.com/sso/identity/login?channel=minions&ru=http://localhost:3000/"
                 >Login</a>
+                {/* onClick={ } 
 
-                {/* <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" /> */}
+                {/* <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" /> 
+              </IconButton> */}
+
+              <IconButton>
+                {isLoggedIn ? "LoggedIn" :
+                  <a
+                    href="https://jssostg.indiatimes.com/sso/identity/login?channel=minions&ru=http://localhost:3000/"
+                  >Login</a>
+
+                }
               </IconButton>
+
             </Tooltip>
             <Menu
               sx={{ mt: '45px' }}

938c53495c9ccc2bde9a0c726ca309f014f127df
souravnitkkr1 <kumar.sourav@timesinternet.in>
2022-01-10 20:20:01 +0530

    2 images in carausal added

--- src/routers/Home.js
+++ src/routers/Home.js
@@ -12,8 +12,8 @@ import { Title } from "@mui/icons-material";
 const breakPoints = [
   { width: 1, itemsToShow: 1 },
   { width: 550, itemsToShow: 2 },
-  { width: 768, itemsToShow: 3 },
-  { width: 1200, itemsToShow: 4 },
+  // { width: 768, itemsToShow: 3 },
+  // { width: 1200, itemsToShow: 4 },
 ];
 
 
